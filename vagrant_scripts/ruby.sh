7b3de31e883af5224f04c6b694ef34f8e32f674b
Peter Eichman <peichman@umd.edu>
2018-12-12 11:59:50 -0500

    Provisioning script improvements.
    * Made the scripts executable.
    * Made the Ruby script re-runnable.
    * Use variables for versions and paths.

--- vagrant_scripts/ruby.sh
+++ vagrant_scripts/ruby.sh
@@ -1,13 +1,27 @@
-echo 'export PATH=$HOME/.rbenv/bin:$PATH' >> $HOME/.bashrc
-echo 'eval "$(rbenv init -)"' >> $HOME/.bashrc
+#!/bin/bash
 
+RUBY_VERSION=2.5.3
+APP_DIR=/vagrant
+
+# setup .bashrc for rbenv
+if ! grep 'rbenv init' $HOME/.bashrc; then
+    cat >>$HOME/.bashrc <<'END'
+export PATH=$HOME/.rbenv/bin:$PATH
+eval "$(rbenv init -)"
+END
+fi
+
+# install rbenv
 export PATH=$HOME/.rbenv/bin:$PATH
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
 eval "$(rbenv init -)"
-rbenv install 2.5.3
-rbenv shell 2.5.3
+
+# install a Ruby
+rbenv install --skip-existing "$RUBY_VERSION"
+rbenv shell "$RUBY_VERSION"
 gem install bundler
 
-cd /vagrant
+# install app dependencies
+cd "$APP_DIR"
 bundle install
 rbenv rehash
--- vagrant_scripts/system.sh
+++ vagrant_scripts/system.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 yum update -y
 yum install -y gcc git openssl-devel readline-devel zlib-devel sqlite-devel \
     java-1.8.0-openjdk gcc-c++ curl
